80956e7e18cae0d4f742702d3f1bd763d064a10b
Gary Ascuy <gary.ascuy@gmail.com>
2018-02-05 23:18:33 -0400

    Adding build

---
+++ dist/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.load = load;
+
+var _lodash = require('lodash');
+
+var _loader = require('./loader');
+
+/**
+ * Loads and merge the settings.
+ * @param {object} options - settings options. 
+ */
+function load(_ref) {
+  var defaults = _ref.defaults,
+      variables = _ref.variables,
+      separator = _ref.separator,
+      regex = _ref.regex,
+      buildValue = _ref.buildValue,
+      json = _ref.json;
+
+  var objectDefaults = (0, _lodash.isString)(defaults) ? (0, _loader.toObject)(defaults, json) : defaults;
+  return (0, _lodash.merge)(objectDefaults, (0, _loader.build)(variables, separator, regex, buildValue));
+}
+
+//
+// import { load } from '@gplatform/settings'
+
+// const defaults = {
+//   appName: 'something',
+//   version: 'Test'
+// }
+
+// const options = {
+//   file: 'settings.json' | { object } | settings
+//   base: asd || 'PRODUCTION_SETTINGS_' 
+// }
+
+// module.exports = load(options)
+
+
+// import { appName } from './settings'
---
+++ dist/loader.js
@@ -0,0 +1,101 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.build = build;
+exports.toObject = toObject;
+exports.defaultBuildValue = defaultBuildValue;
+exports.processSettings = processSettings;
+exports.buildSettings = buildSettings;
+/**
+ * Builds settings from variables object.
+ * @param {object} variables - object variables.
+ * @param {string} separator - fields object separator.
+ * @param {regex} regex - filter for variables.
+ * @param {function} buildValue - build value from string.
+ */
+function build(variables) {
+  var separator = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '_';
+  var regex = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : /^GPLATFORM_SETTINGS_/;
+  var buildValue = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : defaultBuildValue;
+
+  var filteredVariables = processSettings(variables, regex, buildValue);
+  return buildSettings(filteredVariables, separator);
+}
+
+/**
+ * Bulds an objet from string settings.
+ * @param {string} defaults - file path or json string.
+ * @param {boolean} json - true if defaults is json.
+ */
+function toObject(defaults, json) {
+  return json ? JSON.parse(defaults) : require(defaults);
+}
+
+var json = /^(\[|\{).*(\]|\})$/;
+
+function defaultBuildValue(value) {
+  return json.test((value || '').trim()) ? JSON.parse(value) : value;
+}
+
+function processSettings(variables) {
+  var regex = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : /^GPLATFORM_SETTINGS_/;
+  var buildValue = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : defaultBuildValue;
+
+  var result = {};
+  for (var key in variables) {
+    if (regex.test(key)) {
+      result[key.replace(regex, '')] = buildValue(variables[key]);
+    }
+  }
+  return result;
+}
+
+/**
+ * Builds an object using separators for nested fields.
+ * @param {object} variables - list of varibles.
+ */
+function buildSettings(variables) {
+  var separator = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '_';
+
+  var settings = {};
+  for (var key in variables) {
+    var value = variables[key];
+    var fields = key.split(separator);
+
+    var base = settings;
+    var last = null;
+    var field = null;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = fields[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        field = _step.value;
+
+        last = base;
+        base[field] = base[field] || {};
+        base = base[field];
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+
+    last[field] = value;
+  }
+
+  return settings;
+}
